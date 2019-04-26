08554b4110d51c8d0c8065f02eb50179abc61a59
Xi Zhu <zxjucy2012@gmail.com>
2019-04-26 08:16:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 function happyHolidaysTo() {
-  return `Happy holidays, ${name}`
+  return `Happy holidays, ${name}!`
 }
 happyHolidaysTo("Xi")
